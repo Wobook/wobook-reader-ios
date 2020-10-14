486ddeb0e8ce377184b2a49f792e8c30d872a720
Loïc Lavergne <1984800+loiclavergne@users.noreply.github.com>
2020-10-14 18:41:28 +0200

    update package json 3

--- Package.swift
+++ Package.swift
@@ -14,6 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "PressKit",
             url: "https://github.com/Wobook/wobook-reader-ios/releases/download/v3/PressKit.xcframework.zip",
+            checksum: "6662b764861ef8e17e896214f547c76e"
         )
     ]
 )
